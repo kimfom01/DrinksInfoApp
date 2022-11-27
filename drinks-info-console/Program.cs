e85b64d6872da59dbfccc9b09e5a4595189e6bc0
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-11-27 19:07:44 +0000

    Test the current state of the implementation

--- drinks-info-console/Program.cs
+++ drinks-info-console/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using drinks_info_console.APIConsumerServices;
+
 Console.WriteLine("Hello, World!");
+
+var categoryList = await DrinksProcessor.GetCategoriesListAsync();
+
+foreach (var category in categoryList)
+{
+    Console.WriteLine(category.CategoryName);
+}
+
+
+// list of categories
+// www.thecocktaildb.com/api/json/v1/1/list.php?c=list
+
+// filter by category to get drinks
+// https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink
+
+// fetch drink details by id
+// https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007
