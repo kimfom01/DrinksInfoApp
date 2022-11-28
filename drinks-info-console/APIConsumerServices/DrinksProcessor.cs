1323aa9a40fd20889ebe376d08e0638dfbe40736
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-11-28 16:04:30 +0000

    Implement GetDrinks method

--- drinks-info-console/APIConsumerServices/DrinksProcessor.cs
+++ drinks-info-console/APIConsumerServices/DrinksProcessor.cs
@@ -20,4 +20,20 @@ public class DrinksProcessor
 
         return categoryList;
     }
+
+    internal static async Task<List<Drink>> GetDrinksListAsync(string category)
+    {
+        ApiClientHelper.InitializeClient();
+
+        List<Drink> drinkList = new List<Drink>();
+
+        using (var stream = await ApiClientHelper.Client!.GetStreamAsync($"https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={category}"))
+        {
+            var drinks = await JsonSerializer.DeserializeAsync<Drinks>(stream);
+
+            drinkList = drinks!.DrinkList!;
+        }
+
+        return drinkList;
+    }
 }
