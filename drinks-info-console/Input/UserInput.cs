65c5aaa80d21ae8305138844e4dbdc7fc88c2edf
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-11-28 09:30:05 +0000

    Add use input class to handle inputs

---
+++ drinks-info-console/Input/UserInput.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace drinks_info_console.Input;
+
+public class UserInput
+{
+    public string GetInput()
+    {
+        return Console.ReadLine();
+    }
+}
