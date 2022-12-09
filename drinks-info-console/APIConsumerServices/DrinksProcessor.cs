d36d502c9c99c865f34d1b0a3f8c650cfc814baf
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-09 19:55:28 +0300

    Inject httpclient to drinks processor class

--- drinks-info-console/APIConsumerServices/DrinksProcessor.cs
+++ drinks-info-console/APIConsumerServices/DrinksProcessor.cs
@@ -1,3 +1,4 @@
+using System.Net.Http.Headers;
 using System.Text.Json;
 using drinks_info_console.Models;
 
@@ -5,13 +6,21 @@ namespace drinks_info_console.APIConsumerServices;
 
 public class DrinksProcessor
 {
-    public static async Task<List<Category>> GetCategoriesListAsync()
+    private HttpClient _client;
+    public DrinksProcessor(HttpClient client)
+    {
+        _client = client;
+        _client.DefaultRequestHeaders.Accept.Clear();
+        _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+    public async Task<List<Category>> GetCategoriesListAsync()
     {
         ApiClientHelper.InitializeClient();
 
         List<Category> categoryList = new List<Category>();
 
-        using (var stream = await ApiClientHelper.Client!.GetStreamAsync("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"))
+        using (var stream = await _client.GetStreamAsync("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"))
         {
             var categories = await JsonSerializer.DeserializeAsync<Categories>(stream);
 
@@ -21,13 +30,13 @@ public class DrinksProcessor
         return categoryList;
     }
 
-    internal static async Task<List<Drink>> GetDrinksListAsync(string category)
+    public async Task<List<Drink>> GetDrinksListAsync(string category)
     {
         ApiClientHelper.InitializeClient();
 
         List<Drink> drinkList = new List<Drink>();
 
-        using (var stream = await ApiClientHelper.Client!.GetStreamAsync($"https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={category}"))
+        using (var stream = await _client.GetStreamAsync($"https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={category}"))
         {
             var drinks = await JsonSerializer.DeserializeAsync<Drinks>(stream);
 
@@ -37,13 +46,13 @@ public class DrinksProcessor
         return drinkList;
     }
 
-    internal static async Task<List<DrinkDetail>> GetDrinksDetailsAsync(string id)
+    public async Task<List<DrinkDetail>> GetDrinksDetailsAsync(string id)
     {
         ApiClientHelper.InitializeClient();
 
         List<DrinkDetail> drinkDetailList = new List<DrinkDetail>();
 
-        using (var stream = await ApiClientHelper.Client!.GetStreamAsync($"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={id}"))
+        using (var stream = await _client.GetStreamAsync($"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={id}"))
         {
             var details = await JsonSerializer.DeserializeAsync<DrinkDetails>(stream);
 
